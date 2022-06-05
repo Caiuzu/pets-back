0aadc1a4c6a145da602bb46ed98a6a363a719f10
Caio Souza <caio.7s@outlook.com>
2022-06-04 23:57:31 -0300

    feat: Criando entidade Pet

---
+++ src/main/java/br/com/adopt/petsback/domain/entity/Pet.java
@@ -0,0 +1,35 @@
+package br.com.adopt.petsback.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String history;
+
+    @Column(nullable = false)
+    private String photo;
+
+}
