a6c63dc4f682d47468b4c35a88aaf042f4d1f5af
Caio Souza <caio.7s@outlook.com>
2022-06-05 19:01:00 -0300

    feat: Adicionando método de busca por id optional

--- src/main/java/br/com/adopt/petsback/repository/PetRepository.java
+++ src/main/java/br/com/adopt/petsback/repository/PetRepository.java
@@ -3,5 +3,16 @@ package br.com.adopt.petsback.repository;
 import br.com.adopt.petsback.domain.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.EntityNotFoundException;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    default Pet findByIdOrElseThrow(Long id) {
+        var petOptional = findById(id);
+
+        if (petOptional.isPresent()) {
+            return petOptional.get();
+        }
+        throw new EntityNotFoundException("Pet not found");
+    }
+
 }
