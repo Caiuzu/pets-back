279f59a9eda51c6c0d3d1139fdd77fc0cdf71f5e
Caio Souza <caio.7s@outlook.com>
2022-06-05 19:01:55 -0300

    feat: Criando Entidade Adoption

---
+++ src/main/java/br/com/adopt/petsback/domain/entity/Adoption.java
@@ -0,0 +1,37 @@
+package br.com.adopt.petsback.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Adoption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    private Pet pet;
+
+}
