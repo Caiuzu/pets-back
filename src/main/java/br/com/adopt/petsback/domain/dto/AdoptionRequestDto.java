e832e8bb1e8e4100dc9c184d34a3061af44a2ba7
Caio Souza <caio.7s@outlook.com>
2022-06-10 22:50:58 -0300

    feat: Adicionando validação de dados

--- src/main/java/br/com/adopt/petsback/domain/dto/AdoptionRequestDto.java
+++ src/main/java/br/com/adopt/petsback/domain/dto/AdoptionRequestDto.java
@@ -1,22 +1,42 @@
 package br.com.adopt.petsback.domain.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import br.com.adopt.petsback.validator.PetExistsById;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonNaming(SnakeCaseStrategy.class)
 public class AdoptionRequestDto implements Serializable {
 
+    @Email
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
     private String email;
+
+    @NotNull
+    @Min(10)
+    @Max(100)
     private BigDecimal value;
+
+    @NotNull
+    @Positive
+    @PetExistsById
     private Long petId;
 
 }
