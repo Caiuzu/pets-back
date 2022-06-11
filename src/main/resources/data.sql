68b0dd5835c2a2f0a233e2e66647d2cd4dc105bb
Caio Souza <caio.7s@outlook.com>
2022-06-10 23:11:59 -0300

    chore: Adicionando massa de dado de teste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,17 @@
-INSERT INTO PET (ID, NAME, HISTORY, PHOTO) VALUES ('1', 'bilu', 'lala', 'https://razoesparaacreditar.com/wp-content/uploads/2021/05/cachorro-deformidades-1.jpg');
-INSERT INTO PET (ID, NAME, HISTORY, PHOTO) VALUES ('2', 'curumin', 'lolo', 'https://img.r7.com/images/cachorro-que-parece-pompom-13012022171412951?dimensions=771x420');
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('1', 'Bilu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://razoesparaacreditar.com/wp-content/uploads/2021/05/cachorro-deformidades-1.jpg');
+
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('2', 'Curumin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://img.r7.com/images/cachorro-que-parece-pompom-13012022171412951?dimensions=771x420');
+
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('3', 'Cururu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://mega.ibxk.com.br/2015/06/29/29183235917458.jpg');
+
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('4', 'Chupisca', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://s2.glbimg.com/1nvJAz6bSoVQ-LieSHPaNzcwlcY=/e.glbimg.com/og/ed/f/original/2018/07/06/tuna.jpg');
+
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('5', 'Chupaku', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://razoesparaacreditar.com/wp-content/uploads/2021/05/cachorro-deformidades-1.jpg');
+
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO)
+VALUES ('6', 'Calvin', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'https://pbs.twimg.com/profile_images/1479284343273140227/jnO7MNMt_400x400.jpg');
